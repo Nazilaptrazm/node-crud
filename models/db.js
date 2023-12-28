b83e4fd7c21bac3c7aff30d87775f7af6e2caff1
Fauzan <ojanaja03@gmail.com>
2023-12-28 14:44:49 +0700

    first commit

---
+++ models/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+
+        console.log('Database Connected:', mongoose.connection.host);
+    } catch (error) {
+        console.error('Database Connection Error:', error.message);
+    }
+};
+
+module.exports = connectDB;
+
+require('./pegawai.model');
